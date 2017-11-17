c34f91f7342585e1eb33294567dfbb3154b0001c
charles <charles.fortier.12@ulaval.ca>
2017-11-17 11:33:27 -0500

    Prise de mesure fonctionnelle avec sampling hadoc, et début de l'utilisation de d'une sélection bayésienne avec chocolate

--- Main.py
+++ Main.py
@@ -20,11 +20,13 @@ Exécution principale de la prise de données suivie de l'Extraction du score pu
 '''
 
 #Prise de données
+#pip install git+https://github.com/AIworx-Labs/chocolate@master
 from GenerateGaussianList import generate_sequence
 from Screenshot import capture_box, calibrate_screenshot
 from spotShapeDetec import round_score
 import time
 import numpy as np
+import chocolate as choco
 
 def set_box_inputs(JustScreenshot = False):
     if JustScreenshot:
@@ -56,32 +58,73 @@ def set_zernik_polynomial(weights):
     '''
     pass
 
-
-if __name__ == "__main__":
-    #initialisation
-    mean = [1,1,1,1,1,1,1,1,1,1]
-    variances = [4,5,4,5,4,5,4,5,4,5]
+def example_run_hadoc():
+    # initialisation
+    mean = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    variances = [4, 5, 4, 5, 4, 5, 4, 5, 4, 5]
     dimension = len(mean)
-    number = 5
+    number = 20
 
-    #x1, y1, x2, y2 = 200,200,1000,700
-    x1, y1, x2, y2 = set_box_inputs(JustScreenshot=True)
+    x1, y1, x2, y2 = 276, 342, 726, 794
+    # x1, y1, x2, y2 = set_box_inputs(JustScreenshot=True)
 
-
-    test_points = generate_sequence(mean,variances, dimension, number)
-
-    #Prise de données
-    for point in range(test_points.shape[1]):
-
+    print("generating test data")
+    test_points = generate_sequence(mean, variances, dimension, number)
+    print("gathering data")
+    # Prise de données
+    print(test_points.shape)
+    for point in range(test_points.shape[0]):
         set_zernik_polynomial(test_points[point])
 
-        time.sleep(1) #pour que le slm change de forme
-        capture_box(x1, y1, x2, y2,"image{}".format(point))
+        time.sleep(1)  # pour que le slm change de forme
+        capture_box(x1, y1, x2, y2, "image{}".format(point), directory="ScreenCaps")
         time.sleep(0.2)
+        print("\rprogress : {}%".format(100 * point / test_points.shape[0]), end="")
 
-    #extraction du score
+    # extraction du score
+    print("\rextracting score")
     score_list = []
-    for point in range(test_points.shape[1]):
-        score_list.append(round_score("ScreenCaps/image{}.jpg".format(point),"ScreenCaps_contour/image{}contour.jpg".format(point),save_calibration= True))
+    for point in range(test_points.shape[0]):
+        print("\rprogress : {}%".format(100 * point / test_points.shape[0]), end="")
+        score_list.append(round_score("ScreenCaps/image{}.jpg".format(point), "image{}contour.jpg".format(point),
+                                      save_calibration=True))
     np.savetxt("Score_list", score_list)
-    #Fitting
+    # Fitting
+
+def example_run_bayesian():
+    # initialisation
+    mean = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    variances = [4, 5, 4, 5, 4, 5, 4, 5, 4, 5]
+    dimension = len(mean)
+    number = 20
+    space = {"score" : choco.uniform(0,1)}
+    for x in range(dimension):
+        space["{}".format(x)] = choco.uniform(mean[x] - variances[x]/2,mean[x] + variances[x]/2)
+
+
+    #space = space = {"x": choco.uniform(-5, 5),
+    #     "y": choco.uniform(-2, 3)}
+    #connection =
+    #choco.Bayes(connection,space)
+
+def extract_score(number,x1,y1,x2,y2,test_point):
+    '''
+    Measures the score of a given point
+    :param number: number of the point (for filename)
+    :param x1: Int: Coordonnée x du coin supérieur gauche de la boîte à capturer [pixels]
+    :param y1: Int: Coordonnée y du coin supérieur gauche de la boîte à capturer [pixels]
+    :param x2: Int: Coordonnée x du coin inférieur droit de la boîte à capturer [pixels]
+    :param y2: Int: Coordonnée y du coin inférieur droit de la boîte à capturer [pixels]
+    :param test_point: Poids des polynomes de zernick à tester
+    :return:
+    '''
+    set_zernik_polynomial(test_point)
+
+    time.sleep(1)  # pour que le slm change de forme
+    capture_box(x1, y1, x2, y2, "image{}".format(number), directory="ScreenCaps")
+    time.sleep(0.2)
+    score = round_score("ScreenCaps/image{}.jpg".format(number), "image{}contour.jpg".format(point),
+                                  save_calibration=True)
+    return score
+if __name__ == "__main__":
+    example_run_hadoc()
